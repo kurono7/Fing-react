1f242297c023c7970f79f53b046251b3ccec9d75
Omar Orlando Aponte <omar.orlando.aponte@ibm.com>
2022-09-26 19:44:06 -0500

    update twitter

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -1,28 +1,62 @@
 import { Request, Response, NextFunction } from "express";
 import axios, { AxiosResponse } from "axios";
+import { TwitterApi } from 'twitter-api-v2';
 
 interface Post {
-    userId:Number,
+    userId: Number,
     id: Number,
     tittle: String,
     body: String
 }
 
-
-const getPosts =async (req: Request, res: Response, next: NextFunction ) => {
-    try{
-        let result :AxiosResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-        let posts: [Post] = result.data;
-        return res.status(200).json({
-            message:posts
-        })
-    }catch(e){
-        return res.status(404).json({
-            message:e
-        })
-    }
-
-
+interface Client {
+    appKey: String,
+    appSecret: String,
+    accessToken: String,
+    accessSecret: String
 }
 
-export default { getPosts }
+interface TwittSearch {
+    id:String,
+    text:String
+}
+
+const client = new TwitterApi({
+    appKey: 'LeYiKaHCGCcNzuflzEVX7UVCp',
+    appSecret: 'PAcYRY665YHVBYr6JcPEpqbjbcFOxE8WyDEzFV5CwC1DkVGMX7',
+    accessToken: '901500680015409152-i2zro9cMyw8YWthC3JHy0b8PgcDm6m9',
+    accessSecret: 'L4rdwfjue9QogiGEvRb87X2RE2FrbeAGXjrwog6VucZdY'
+})
+
+
+const getPosts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let result: AxiosResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+        let posts: [Post] = result.data;
+        return res.status(200).json({
+            message: posts
+        })
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+
+const postTwitts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let query = req.body.text;
+        let result: AxiosResponse = await client.v2.get('tweets/search/recent', { query: query, max_results: 100 });
+        let twits:[TwittSearch] = result.data;
+        return res.status(200).json({
+            message:twits
+        })
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+export default { getPosts, postTwitts }
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -2,6 +2,8 @@ import express from "express";
 import controller from "../controllers/posts";
 const router = express.Router();
 
-router.get('/posts', controller.getPosts)
+router.get('/posts', controller.getPosts);
+
+router.post('/getSearchTwits', controller.postTwitts);
 
 export = router;
